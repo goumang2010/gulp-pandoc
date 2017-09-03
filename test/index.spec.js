164619067d5b3cfc0ade661fbfd41ee2a79a79d1
goumang2010 <goumang2010@live.com>
2017-09-03 22:13:00 +0800

    index spec

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,15 @@
 /* Modules */
 
-var must   = require('must');
+var chai = require('chai');
+var should = chai.should();
 var gutil  = require('gulp-util');
-var pandoc = require('./index');
+var pandoc = require('../index');
 
 /* Tests */
 
 describe('pandoc()', function() {
 
-  it('must throw an error if pdc args are incorrect', function(done) {
+  it('should throw an error if pdc args are incorrect', function(done) {
     var file = new gutil.File({
       path: 'test.md',
       contents: new Buffer('"This is a test."')
@@ -22,14 +23,14 @@ describe('pandoc()', function() {
     });
 
     stream.on('error', function(err) {
-      err.must.include('Error: pandoc exited with code 9');
+      err.should.include('Error: pandoc exited with code 9');
       done();
     });
 
     stream.write(file);
   });
 
-  it('must compile Markdown to HTML', function(done) {
+  it('should compile Markdown to HTML', function(done) {
     var file = new gutil.File({
       path: 'test.md',
       contents: new Buffer('"This is a test."')
@@ -46,8 +47,8 @@ describe('pandoc()', function() {
 
     stream.on('data', function(file) {
       var content = file.contents.toString().trimRight(); // [1]
-      file.relative.must.be('test.html');
-      content.must.be('<p>“This is a test.”</p>');
+      file.relative.should.equal('test.html');
+      content.should.equal('<p>“This is a test.”</p>');
       done();
     });
 
