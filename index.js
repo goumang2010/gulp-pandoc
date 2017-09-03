db5e4cee49066ca3d6bc4e9c1c3158877985d297
goumang2010 <goumang2010@live.com>
2017-09-03 22:12:00 +0800

    refactor main code

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
 /* Modules */
 
-var pdc     = require('pdc');
 var through = require('through2');
-var gutil   = require('gulp-util');
+var pandoc = require('./pandoc');
+var gutil = require('gulp-util');
 
 /* Plugin */
 
 var PluginError = gutil.PluginError;
-var PluginName  = 'gulp-pandoc';
+var PluginName = 'gulp-pp-pandoc';
 
 /* Exports */
 
-module.exports = function(opts) {
+module.exports = function (opts) {
+  var macro = opts.macro;
   var from = opts.from;
-  var to   = opts.to;
-  var ext  = opts.ext;
+  var to = opts.to;
+  var ext = opts.ext;
   var args = opts.args || [];
 
-  if (!from) { throw new PluginError(PluginName, '"from" is not defined'); }
-  if (!to) { throw new PluginError(PluginName, '"to" is not defined'); }
-  if (!ext) { throw new PluginError(PluginName, '"ext" is not defined'); }
+  if (!from) {
+    throw new PluginError(PluginName, '"from" is not defined');
+  }
+  if (!to) {
+    throw new PluginError(PluginName, '"to" is not defined');
+  }
+  if (!ext) {
+    throw new PluginError(PluginName, '"ext" is not defined');
+  }
 
   return through.obj(function (file, enc, cb) {
-    var input = file.contents.toString();
-    if (file.isNull())  { 
+    var input = file.contents;
+    if (file.isNull()) {
       this.push(file);
       return cb();
     }
-    
     if (file.isStream()) {
-      this.emit('error', new PluginError(PluginName, 'Streaming not supported')); 
+      this.emit('error', new PluginError(PluginName, 'Streaming not supported'));
       return cb();
     }
+    var pdcProcess = pandoc(from, to, args);
+    pdcProcess.stdin.end(input);
+    var chunks = [];
+    var size = 0;
 
-    pdc(input, from, to, args, function(err, output) {
-      if (err) {
-        this.emit('error', err.toString());
+    pdcProcess.stdout.on('data', function (data) {
+      chunks.push(data);
+      size += data.length;
+    });
+    // listen on exit
+    pdcProcess.on('close', (code) => {
+      if (code !== 0) {
+        this.emit('error', 'Error: pandoc exited with code ' + code);
         return cb();
       }
-      file.contents = new Buffer(output);
+      var result = Buffer.concat(chunks, size);
+      file.contents = result;
       file.path = gutil.replaceExtension(file.path, opts.ext);
       this.push(file);
       return cb();
-    }.bind(this));
+    })
+    pdcProcess.on('error', function (err) {
+      this.emit('error', err.toString());
+      return cb();
+    });
   });
 };
